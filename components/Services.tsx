e459b7782b371f8d1f474d584429ce9bcd41986c
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-11-14 14:20:54 +0400

    c

--- components/Services.tsx
+++ components/Services.tsx
@@ -4,38 +4,40 @@ import Image from 'next/image';
 
 export default function Services() {
   return (
-    <div>
+    <div className='max-w-[1360px] w-full  flex justify-between items-center '>
       {servicesData.map((item) => (
-        <div className='relative'>
+        <div className='relative w-[440px] h-[300px] flex flex-col justify-center items-center'>
           <div>
             <Image
               src={item.vectorLeft}
               alt='vector'
-              width={0}
-              height={0}
-              className='absolute w-auto h-auto object-cover'
+              width={153}
+              height={207}
+              className='absolute  object-cover top-0 left-0'
             />
           </div>
           <div>
             <Image
               src={item.vectorRight}
               alt='vector'
-              width={0}
-              height={0}
-              className='absolute w-auto h-auto object-cover'
+              width={153}
+              height={207}
+              className='absolute  object-cover top-0 right-0'
             />
           </div>
           <div>
             <Image
               src={item.photo}
-              alt='vector'
-              width={0}
-              height={0}
-              className='w-auto h-auto object-cover'
+              alt='img'
+              width={200}
+              height={200}
+              className='object-cover'
             />
           </div>
           <div>
-            <h1>{item.title}</h1>
+            <h1 className='font-popins font-[275] text-[32px] leading-[48px]'>
+              {item.title}
+            </h1>
           </div>
         </div>
       ))}
--- constants/servisesData.ts
+++ constants/servisesData.ts
@@ -9,19 +9,19 @@ export const servicesData: dataObj[] = [
   {
     title: 'NET ZERO',
     photo: '/assets/img/circular.png',
-    vectorLeft: '/Vetor-left-up.webp',
-    vectorRight: '/Vector-right-up.webp',
+    vectorLeft: '/assets/img/Vetor-left-up.webp',
+    vectorRight: '/assets/img/Vector-right-up.webp',
   },
   {
     title: 'ESG',
-    photo: '/esg.webp',
-    vectorLeft: '/Vetor-left-down.webp',
-    vectorRight: '/Vector-right-down.webp',
+    photo: '/assets/img/esg.webp',
+    vectorLeft: '/assets/img/Vetor-left-down.webp',
+    vectorRight: '/assets/img/Vector-right-down.webp',
   },
   {
     title: 'Circular Economy',
-    photo: '/circular.webp',
-    vectorLeft: '/Vetor-left-up.webp',
-    vectorRight: '/Vector-right-up.webp',
+    photo: '/assets/img/circular.webp',
+    vectorLeft: '/assets/img/Vetor-left-up.webp',
+    vectorRight: '/assets/img/Vector-right-up.webp',
   },
 ];
